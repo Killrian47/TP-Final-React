38b3b6c0cb8c755b25a5ac91a239944c394d4500
Killian PORTIER <killianportier47@yahoo.com>
2023-12-14 13:49:03 +0100

    404 page

--- src/page/PathNotFound.jsx
+++ src/page/PathNotFound.jsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+
 const PathNotFound = () => {
     return (
-        <h1>404 page</h1>
+        <>
+            <div class="d-flex align-items-center justify-content-center vh-100 url-not-found">
+                <div class="text-center">
+                    <h1 class="display-1 fw-bold">404</h1>
+                    <p class="fs-3"> <span class="text-danger">Dommage !</span> Page non trouvée</p>
+                    <p class="lead">
+                        La page que vous cherchez n'existe pas
+                    </p>
+                    <Link className="btn btn-primary" to='/'>Retour à la page d'accueil</Link>
+                </div>
+            </div>
+        </>
     )
 }
 
